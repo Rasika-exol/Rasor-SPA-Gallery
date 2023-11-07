d56d7a687766c01ae92f9fdab0f55af5f7894f8d
Svitlana <rsv0011@gmail.com>
2023-11-07 16:54:31 +0100

    feat: implement the search function

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,22 @@
 import "./style.css";
 import fetchImage from "./fetchContent";
 import renderImages from "./renderContent";
-
+import searchImages from "./searchImages";
 const imageContainerEl = document.querySelector(
   ".image-container"
 ) as HTMLElement;
+
+const searchButtonEl = document.querySelector(
+  "#search_button"
+) as HTMLButtonElement;
 
 const main = async () => {
   const data = await fetchImage();
   renderImages(data, imageContainerEl);
 };
 
+searchButtonEl.addEventListener("click", async () => {
+  const queryRes = await searchImages();
+  renderImages(queryRes, imageContainerEl);
+});
 main();
