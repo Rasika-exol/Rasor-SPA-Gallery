0901585a0a10787eb9a8f7d7d987755e104604df
Antti Jankeri <antti.jankeri@gmail.com>
2023-11-07 16:35:28 +0100

    tests: jsdom works...?

--- src/main.ts
+++ src/main.ts
@@ -1,62 +1,14 @@
 import "./style.css";
-
-const ACCESS_KEY = import.meta.env.VITE_ACCESS_KEY;
-
-// document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-// <nav class="nav">
-//       <div class="nav__logo">
-//         <p>Logo</p>
-//       </div>
-//       <div class="nav__link">
-//         <p>Github Link</p>
-//       </div>
-//     </nav>
-//     <main>
-//       <search class="search">
-//         <icon class="search__icon"></icon>
-//         <input class="search__input" type="text" />
-//       </search>
-//       <content class="image-container">
-
-//       </content>
-//       <div class="menu-buttons">
-//         <button class="menu-buttons__button">Prev</button
-//         ><button class="menu-buttons__button">Next</button>
-//       </div>
-//     </main>
-// `;
+import fetchImage from "./fetchContent";
+import renderImages from "./renderContent";
 
 const imageContainerEl = document.querySelector(
   ".image-container"
 ) as HTMLElement;
 
-const fetchImage = async () => {
-  const response = await fetch(
-    `https://api.unsplash.com/photos/?client_id=${ACCESS_KEY}`
-  );
-  const images = await response.json();
-  console.log(images);
-
-  return images;
-};
-
-const renderImages = (
-  data: [{ id: number; urls: { small: string }; description: string }]
-) => {
-  const template = data.map((source) => {
-    return ` 
-    <div id=${source.id} class="image-container__image">
-    <img src=${source.urls.small} alt=${source.description} />
-    </div>
-    `;
-  });
-
-  imageContainerEl.innerHTML = template.join("");
-};
-
 const main = async () => {
   const data = await fetchImage();
-  renderImages(data);
+  renderImages(data, imageContainerEl);
 };
 
 main();
---
+++ src/renderContent.ts
@@ -0,0 +1,16 @@
+const renderImages = (
+  data: { id: number; urls: { small: string }; description: string }[],
+  container: HTMLElement
+) => {
+  const template = data.map((source) => {
+    return ` 
+    <div id=${source.id} class="image-container__image">
+    <img src=${source.urls.small} alt=${source.description}>
+    </div>
+    `;
+  });
+
+  container.innerHTML = template.join("");
+};
+
+export default renderImages;
