cefb01dd805710a74828856958c44a51e7fbfd0b
Antti Jankeri <antti.jankeri@gmail.com>
2023-11-07 13:34:02 +0100

    feat: html doneish

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,57 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+import { setupCounter } from "./counter.ts";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+<nav class="nav">
+      <div class="nav__logo">
+        <p>Logo</p>
+      </div>
+      <div class="nav__link">
+        <p>Github Link</p>
+      </div>
+    </nav>
+    <main>
+      <search class="search">
+        <icon class="search__icon"></icon>
+        <input class="search__input" type="text" />
+      </search>
+      <content class="image-container">
+        <div class="image-container__image">
+          <img src="https://picsum.photos/300/200" alt="image 1" />
+        </div>
+        <div class="image-container__image">
+          <img src="https://picsum.photos/300/200" alt="image 2" />
+        </div>
+        <div class="image-container__image">
+          <img src="https://picsum.photos/300/200" alt="image 3" />
+        </div>
+        <div class="image-container__image">
+          <img src="https://picsum.photos/300/200" alt="image 4" />
+        </div>
+        <div class="image-container__image">
+          <img src="https://picsum.photos/300/200" alt="image 5" />
+        </div>
+        <div class="image-container__image">
+          <img src="https://picsum.photos/300/200" alt="image 6" />
+        </div>
+        <div class="image-container__image">
+          <img src="https://picsum.photos/300/200" alt="image 7" />
+        </div>
+        <div class="image-container__image">
+          <img src="https://picsum.photos/300/200" alt="image 8" />
+        </div>
+        <div class="image-container__image">
+          <img src="https://picsum.photos/300/200" alt="image 9" />
+        </div>
+        <div class="image-container__image">
+          <img src="https://picsum.photos/300/200" alt="image 10" />
+        </div>
+      </content>
+      <div class="menu-buttons">
+        <button class="menu-buttons__button">Prev</button
+        ><button class="menu-buttons__button">Next</button>
+      </div>
+    </main>
+`;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
