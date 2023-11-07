222ebd7d4784395cf83077208c611e78a0770aea
Svitlana <rsv0011@gmail.com>
2023-11-07 14:22:32 +0100

    feat: fetch the data, render data on the page

--- src/main.ts
+++ src/main.ts
@@ -1,57 +1,64 @@
 import "./style.css";
-import { setupCounter } from "./counter.ts";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-<nav class="nav">
-      <div class="nav__logo">
-        <p>Logo</p>
-      </div>
-      <div class="nav__link">
-        <p>Github Link</p>
-      </div>
-    </nav>
-    <main>
-      <search class="search">
-        <icon class="search__icon"></icon>
-        <input class="search__input" type="text" />
-      </search>
-      <content class="image-container">
-        <div class="image-container__image">
-          <img src="https://picsum.photos/300/200" alt="image 1" />
-        </div>
-        <div class="image-container__image">
-          <img src="https://picsum.photos/300/200" alt="image 2" />
-        </div>
-        <div class="image-container__image">
-          <img src="https://picsum.photos/300/200" alt="image 3" />
-        </div>
-        <div class="image-container__image">
-          <img src="https://picsum.photos/300/200" alt="image 4" />
-        </div>
-        <div class="image-container__image">
-          <img src="https://picsum.photos/300/200" alt="image 5" />
-        </div>
-        <div class="image-container__image">
-          <img src="https://picsum.photos/300/200" alt="image 6" />
-        </div>
-        <div class="image-container__image">
-          <img src="https://picsum.photos/300/200" alt="image 7" />
-        </div>
-        <div class="image-container__image">
-          <img src="https://picsum.photos/300/200" alt="image 8" />
-        </div>
-        <div class="image-container__image">
-          <img src="https://picsum.photos/300/200" alt="image 9" />
-        </div>
-        <div class="image-container__image">
-          <img src="https://picsum.photos/300/200" alt="image 10" />
-        </div>
-      </content>
-      <div class="menu-buttons">
-        <button class="menu-buttons__button">Prev</button
-        ><button class="menu-buttons__button">Next</button>
-      </div>
-    </main>
-`;
+const ACCESS_KEY = import.meta.env.VITE_ACCESS_KEY;
 
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+// document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+// <nav class="nav">
+//       <div class="nav__logo">
+//         <p>Logo</p>
+//       </div>
+//       <div class="nav__link">
+//         <p>Github Link</p>
+//       </div>
+//     </nav>
+//     <main>
+//       <search class="search">
+//         <icon class="search__icon"></icon>
+//         <input class="search__input" type="text" />
+//       </search>
+//       <content class="image-container">
+       
+//       </content>
+//       <div class="menu-buttons">
+//         <button class="menu-buttons__button">Prev</button
+//         ><button class="menu-buttons__button">Next</button>
+//       </div>
+//     </main>
+// `;
+
+const imageContainerEl = document.querySelector(
+  ".image-container"
+) as HTMLElement;
+
+const fetchImage = async () => {
+  const response = await fetch(
+    `https://api.unsplash.com/photos/?client_id=${ACCESS_KEY}`
+  );
+  const images = await response.json();
+  console.log(images);
+
+  return images;
+};
+
+const renderImages = (
+  data: [{ id: number; urls: { small: string }; description: string }]
+) => {
+  const template = data.map((source) => {
+    return ` 
+    <div id=${source.id} class="image-container__image">
+    <img src=${source.urls.small} alt=${source.description} />
+    </div>
+    `;
+  });
+
+  imageContainerEl.innerHTML = template.join("");
+};
+
+const main = async () => {
+  const data = await fetchImage();
+  renderImages(data);
+
+
+};
+
+main();
