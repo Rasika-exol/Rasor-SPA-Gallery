dee371e6049ae9a23e72f41ddcc97c2e5507d509
Ramona <ramona.ekanayake@appliedtechnology.se>
2023-11-08 11:43:39 +0100

    feat:Implemented Local Storage

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,11 @@ inputEl.addEventListener("change", () => {
 
 formEl.addEventListener("submit", async (event: Event) => {
   event.preventDefault();
+  const storedDataJSON = localStorage.getItem("values");
+  const storedData = storedDataJSON ? JSON.parse(storedDataJSON) : [];
+  const changedData = [...storedData, searchValue];
+  localStorage.setItem("values", JSON.stringify(changedData));
+
   const queryRes = await searchImages(searchValue);
   renderImages(queryRes, imageContainerEl);
 });
