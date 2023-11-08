9412f626ec2b7bb753a54e531605fffaf91683a9
anttijankeri <antti.jankeri@gmail.com>
2023-11-08 16:28:00 +0100

    maybe api key

--- src/fetchContent.ts
+++ src/fetchContent.ts
@@ -1,9 +1,10 @@
 const fetchImage = async () => {
   const ACCESS_KEY = import.meta.env.VITE_ACCESS_KEY;
 
-  const response = await fetch(
-    `https://api.unsplash.com/photos/?client_id=${ACCESS_KEY}`
-  );
+  const response = await fetch(`https://api.unsplash.com/photos/`, {
+    headers: { Authorization: "Client-ID " + ACCESS_KEY },
+  });
+
   const images = await response.json();
 
   return images;
--- src/searchImages.ts
+++ src/searchImages.ts
@@ -1,11 +1,15 @@
 const searchImages = async (query: string) => {
   if (query === "") query = "dogs";
 
-  const ACCESS_KEY = process.env.VITE_ACCESS_KEY;
+  const ACCESS_KEY = import.meta.env.VITE_ACCESS_KEY;
 
   const response = await fetch(
-    ` https://api.unsplash.com/search/photos?query=${query}&client_id=${ACCESS_KEY}`
+    `https://api.unsplash.com/photos?query=${query}`,
+    {
+      headers: { Authorization: "Client-ID " + ACCESS_KEY },
+    }
   );
+
   const images = await response.json();
 
   return images.results;
