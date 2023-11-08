59ec9b39cc49c5f8503207098002a245a8d18534
anttijankeri <antti.jankeri@gmail.com>
2023-11-08 16:13:35 +0100

    better env import

--- src/searchImages.ts
+++ src/searchImages.ts
@@ -1,7 +1,7 @@
 const searchImages = async (query: string) => {
   if (query === "") query = "dogs";
 
-  const ACCESS_KEY = import.meta.env.VITE_ACCESS_KEY;
+  const ACCESS_KEY = process.env.VITE_ACCESS_KEY;
 
   const response = await fetch(
     ` https://api.unsplash.com/search/photos?query=${query}&client_id=${ACCESS_KEY}`
